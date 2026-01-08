5529e69756378f47b7d22c017745cde1e43070a4
4j4yk <git@ajayk.me>
2026-01-07 23:13:10 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,10 +3,12 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    sys.stdout.write("$ ")
-    cmd = input() # take input and return as str
-    print(f"{cmd}: command not found")
-    pass
+    while True: #forever loop
+        sys.stdout.write("$ ")
+        cmd = input() # take input and return as str
+        print(f"{cmd}: command not found")
+    #pass
+    return 1
 
 
 if __name__ == "__main__":

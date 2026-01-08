7ef66f06745e6c509c82de092bb0d8392934f8ba
4j4yk <git@ajayk.me>
2026-01-07 22:59:23 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ import sys
 def main():
     # TODO: Uncomment the code below to pass the first stage
     sys.stdout.write("$ ")
+    cmd = input() # take input and return as str
+    print(f"{cmd}: command not found")
     pass
 
 
